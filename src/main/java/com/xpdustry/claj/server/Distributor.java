d5bdd58e86002398286d32505dced9f36e32ac1d
ZetaMap <56844734+ZetaMap@users.noreply.github.com>
2025-04-02 18:06:13 +0200

    Update Distributor.java

--- src/main/java/com/xpdustry/claj/server/Distributor.java
+++ src/main/java/com/xpdustry/claj/server/Distributor.java
@@ -138,8 +138,7 @@ public class Distributor extends Server {
                 if (link.equals("new")) {
                     // Warn host about claj deprecation, see https://github.com/xpdustry/claj-v2
                     connection.sendTCP("[scarlet][[CLaJ Server]: [][yellow]WARNING: []"
-                        + "You using an old version of CLaJ, please update it by installing the 'claj v2' mod, in mod browser. "
-                        + "And optionally removing the 'scheme size' mod (or 'scheme size lite').");
+                        + "You using an old version of CLaJ, please update it by installing the 'claj' mod, in mod browser.");
                   
                     link = generateLink();
 
